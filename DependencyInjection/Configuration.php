31ffc78f697f5349aefb6cb9664e866b8b0746b6
botnyx <chlsbkrs@gmail.com>
2018-05-28 01:45:32 +0200

    Update Configuration.php

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace WMC\SwiftmailerTwigBundle\DependencyInjection;
+namespace botnyx\SwiftmailerTwigBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
